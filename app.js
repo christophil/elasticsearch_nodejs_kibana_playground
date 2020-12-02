9e85e6ba294aa6908e2f425aa03d094eab227df2
christophil <christophil090318@gmail.com>
2020-12-02 17:28:42 +0100

    FINAL COMMIT

--- app.js
+++ app.js
@@ -37,10 +37,7 @@ server.use(cors());
 server.use(bodyParser.json());
 
 
-
 server.get('/', (req, res) => {
-
-    
 
     var error = false;
     var result;
@@ -51,7 +48,6 @@ server.get('/', (req, res) => {
         size: 10000,
         }, function(err, resp, status){
           if(err){
-            console.log(err);
             error_message = err;
             error = true;
           }else{
@@ -60,26 +56,81 @@ server.get('/', (req, res) => {
 
           }
 
+          res.render('index', {error: error, error_message: error_message, result: result});
+      });
+
+    
+});
+
+
+server.get('/visualize/age', (req, res) => {
+
+    var error = false;
+    var result;
+    var error_message;
+
+    client.search({
+        index: 'bank',
+        body: {
+            "aggs": {
+              "2": {
+                "terms": {
+                  "field": "age",
+                  "order": {
+                    "_count": "desc"
+                  },
+                  "size": 5
+                }
+              }
+            },
+            "size": 0,
+            "stored_fields": [
+              "*"
+            ],
+            "script_fields": {},
+            "docvalue_fields": [],
+            "_source": {
+              "excludes": []
+            },
+            "query": {
+              "bool": {
+                "must": [],
+                "filter": [
+                  {
+                    "match_all": {}
+                  }
+                ],
+                "should": [],
+                "must_not": []
+              }
+            }
+          }
+        }, function(err, resp, status){
+          if(err){
+            error_message = err;
+            error = true;
+            res.render('age', {error: error, error_message: error_message});
+          }else{
+            
+            result = resp.hits.hits;
+
+            var values = [];
+
+          resp.aggregations["2"].buckets.forEach(function(elt){
+
+            values.push({ages: elt.key, owners_count: elt.doc_count});
+          });
+
           var yourVlSpec = {
-            $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
-            description: 'A simple bar chart with embedded data.',
+            "width": 500,
+            "height": 500,
             data: {
-              values: [
-                {a: 'A', b: 28},
-                {a: 'B', b: 55},
-                {a: 'C', b: 43},
-                {a: 'D', b: 91},
-                {a: 'E', b: 81},
-                {a: 'F', b: 53},
-                {a: 'G', b: 19},
-                {a: 'H', b: 87},
-                {a: 'I', b: 52}
-              ]
+              values: values
             },
             mark: 'bar',
             encoding: {
-              x: {field: 'a', type: 'ordinal'},
-              y: {field: 'b', type: 'quantitative'}
+              x: {field: 'ages', type: 'ordinal'},
+              y: {field: 'owners_count', type: 'quantitative'}
             }
           };
           let vegaspec = lite.compile(yourVlSpec).spec
@@ -89,46 +140,111 @@ server.get('/', (req, res) => {
           view.toSVG()
       .then(function(svg) {
 
-        res.render('index', {error: error, error_message: error_message, result: result, image: svg});
+        res.render('age', {error: error, error_message: error_message, result: result, image: svg});
 
-        //res.send(svg);
       })
-      .catch(function(err) { console.error(err); });
-
-          
+      .catch(function(err) { 
+        res.render('age', {error: true, error_message: err});
       });
 
-    
+          }
+        }
+    );
+
 });
 
+server.get('/visualize/gender', (req, res) => {
 
-//GET specific bank by id
-server.get('/bank/:id', (req, res) => {
-    var bank;
-    client.get({
+  var error = false;
+  var result;
+  var error_message;
+
+  client.search({
       index: 'bank',
-      type: 'account',
-      id: req.params.id
+      body: {
+        "aggs": {
+          "2": {
+            "terms": {
+              "field": "gender.keyword",
+              "order": {
+                "_count": "desc"
+              },
+              "size": 5
+            }
+          }
+        },
+        "size": 0,
+        "stored_fields": [
+          "*"
+        ],
+        "script_fields": {},
+        "docvalue_fields": [],
+        "_source": {
+          "excludes": []
+        },
+        "query": {
+          "bool": {
+            "must": [],
+            "filter": [
+              {
+                "match_all": {}
+              }
+            ],
+            "should": [],
+            "must_not": []
+          }
+        }
+      }
       }, function(err, resp, status){
         if(err){
-          console.log(err);
+          error_message = err;
+          error = true;
+          res.render('gender', {error: error, error_message: error_message});
         }else{
-          bank = resp._source;
-          console.log('found the requested document', resp);
-          if (!bank){
-            return res.status(400).send({
-              message: 'bank is not found for id ${req.params.id}'
-            });
-          }
-          return res.status(200).send({
-            message: 'Get bank calls for id ${req.params.id} succeeded',
-            bank: bank
-          });
-        }
-    });
-   });
+          
+          result = resp.hits.hits;
 
-//GET specific bank by id
+          var values = [];
+
+        resp.aggregations["2"].buckets.forEach(function(elt){
+
+          values.push({category: elt.key, value: elt.doc_count});
+
+        });
+
+        var yourVlSpec = {
+          "width": 500,
+          "height": 500,
+          "data": {
+            "values": values
+          },
+          "mark": "arc",
+          "encoding": {
+            "theta": {"field": "value", "type": "quantitative"},
+            "color": {"field": "category", "type": "nominal"}
+          },
+          "view": {"stroke": null}
+        };
+        let vegaspec = lite.compile(yourVlSpec).spec
+        var view = new vega.View(vega.parse(vegaspec), 
+        {renderer: "none"})
+  
+        view.toSVG()
+    .then(function(svg) {
+
+      res.render('gender', {error: error, error_message: error_message, result: result, image: svg});
+
+    })
+    .catch(function(err) { 
+      res.render('gender', {error: true, error_message: err});
+    });
+
+        }
+      }
+  );
+
+});
+
 server.get('/search', (req, res) => {
 
     var search = req.query.search;
@@ -141,8 +257,6 @@ server.get('/search', (req, res) => {
         var error = false;
         var result;
         var error_message;
-
-        
 
         client.search({
             index: 'bank',
@@ -164,7 +278,6 @@ server.get('/search', (req, res) => {
             }
         }, function(err, resp, status){
               if(err){
-                console.log(err);
                 error_message = err;
                 error = true;
               }else{
@@ -174,7 +287,6 @@ server.get('/search', (req, res) => {
 
               res.render('index', {error: error, error_message: error_message, result: result});
           });
-
     }
     
 });
@@ -199,7 +311,6 @@ server.get('/to_delete', (req, res) => {
             id: id
         }, function(err, resp, status){
               if(err){
-                console.log(err);
                 error_message = err;
                 error = true;
               }else{
@@ -243,7 +354,6 @@ server.get('/to_edit', (req, res) => {
             id: id
         }, function(err, resp, status){
               if(err){
-                console.log(err);
                 error_message = err;
                 error = true;
               }else{
@@ -281,8 +391,8 @@ server.get('/delete', (req, res) => {
             id: id
         }, function(err, resp, status){
               if(err){
-                console.log(err);
                 error_message = err;
+                error = true;
               }else{
 
                 result = resp._source;
@@ -299,9 +409,8 @@ server.get('/delete', (req, res) => {
 
 server.get('/add', (req, res) => {
 
-
     if(req.query.firstname == undefined || req.query.firstname == null){
-        res.render('index', {error: true, error_message: "Missing filed while adding bank account" , result: null});
+        res.render('index', {error: true, error_message: "Missing fields while adding bank account" , result: null});
     }
     else{
 
@@ -327,8 +436,8 @@ server.get('/add', (req, res) => {
             }
         }, function(err, resp, status){
               if(err){
-                console.log(err);
                 error_message = err;
+                error = true;
               }else{
 
                 result = resp._source;
@@ -350,82 +459,30 @@ server.post('/add_from_csv', (req, res) => {
     form.parse(req, function (err, fields, files) {
       
         if(err){
-            console.log(err);
+          res.render('index', {error: true, error_message: err});
         }
         else{
 
             fs.createReadStream(files.filetoupload.path).pipe(
                 parse({columns: true, delimiter: ";"}, function (err, records) {
         
-                    console.log(records);
-        
                     const body = records.flatMap(doc => [{ index: { _index: 'bank' } }, doc]);
         
                     client.bulk({ refresh: true, body }, function(err, resp, status){
                           if(err){
-                            console.log(err);
-                          }else{
-            
-                            console.log(resp);
+                            res.render('index', {error: true, error_message: err});
 
+                          }else{
+          
                             res.render('index', {error_from_csv: false});
             
                           }
                         
                       });
-        
-                    //console.log(records);
                 })
             );
         }
     });
-    
-    /*
-
-    */
-    
-
-
-    /*if(req.query.firstname == undefined || req.query.firstname == null){
-        res.render('index', {error: true, error_message: "Missing filed while adding bank account" , result: null});
-    }
-    else{
-
-        var error = false;
-        var result;
-        var error_message;
-        var was_added = false;
-    
-        client.index({
-            index: 'bank',
-            body: {
-                account_number: req.query.accountnumber,
-                firstname: req.query.firstname,
-                lastname: req.query.lastname,
-                gender: req.query.gender,
-                age: req.query.age,
-                state: req.query.state,
-                city: req.query.city,
-                address: req.query.address,
-                email: req.query.email,
-                employer: req.query.employer,
-                balance: req.query.balance
-            }
-        }, function(err, resp, status){
-              if(err){
-                console.log(err);
-                error_message = err;
-              }else{
-
-                result = resp._source;
-                was_added = true;
-
-              }
-
-              res.render('add', {was_added: was_added, error: error, error_message: error_message, result: result});
-          });
-
-    } */
     
 });
 
@@ -463,8 +520,8 @@ server.get('/edit', (req, res) => {
             }
         }, function(err, resp, status){
               if(err){
-                console.log(err);
                 error_message = err;
+                error = true;
               }else{
 
                 result = resp._source;
@@ -479,109 +536,6 @@ server.get('/edit', (req, res) => {
     
 });
 
-//GET specific bank by id
-server.get('/bank/', (req, res) => {
-    var bank;
-    client.search({
-      index: 'bank',
-      size: 5,
-      }, function(err, resp, status){
-        if(err){
-          console.log(err);
-        }else{
-          bank = resp.hits.hits;
-          console.log('found the requested document', resp);
-          if (!bank){
-            return res.status(400).send({
-              message: 'bank is not found for id ${req.params.id}'
-            });
-          }
-          return res.json({
-            error: false,
-            body: {
-                message: 'Get bank calls for id ${req.params.id} succeeded',
-                bank: bank
-            }
-          });
-        }
-    });
-   })
-
-
 server.listen(8082);
 
 console.log('Server running at http://127.0.0.1:8082/');
-
-
-
-/*
-
-var workouts = [
-  {
-    id: 1,
-    type: 'weight',
-    duration: 45,
-    date: '02/12/2020'
-  },
-  {
-      id: 2,
-      type: 'weight',
-      duration: 49,
-      date: '01/12/2020'
-    }
-]
-
-//POST specific workout doc
-server.post('/workout', (req, res) => {
-  if(!req.body.id){
-    return res.status(400).send({
-      message: 'Id is required'
-    });
-  }
-  client.index({
-    index: 'workout',
-    type: 'mytype',
-    id: req.body.id,
-    body: req.body
-  }, function(err, resp, status){
-    if(err){
-      console.log(err);
-    }else{
-      return res.status(200).send({
-        message: 'POST workout call succeeded'
-      })
-    }
-  });
-})
-
-
-
-//GET specific shakespeare by id
-server.get('/shakes/:id', (req, res) => {
- var shakespeare;
- client.get({
-   index: 'shakespeare',
-   type: '_doc',
-   id: req.params.id
-   }, function(err, resp, status){
-     if(err){
-       console.log(err);
-     }else{
-       shakespeare = resp._source;
-       console.log('found the requested document', resp);
-       if (!shakespeare){
-         return res.status(400).send({
-           message: 'shakespeare is not found for id ${req.params.id}'
-         });
-       }
-       return res.status(200).send({
-         message: 'Get shakespeare calls for id ${req.params.id} succeeded',
-         shakespeare: shakespeare
-       });
-     }
- });
-})
-
-
-
-*/
